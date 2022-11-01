919b4b26d8540c1a77800932b0bc57fdea0f7ab3
HomeDweller <asm886ith@gmail.com>
2022-11-01 19:56:07 +0100

    fourth commit

--- js/script.js
+++ js/script.js
@@ -220,9 +220,92 @@ function initMap() {
     });
 }
 $(document).ready(function () {
-    $('.articles').slick({
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 3
-    });
+    if (screen.availWidth < 768) {
+        $('.articles').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    }
+    else if (screen.availWidth >= 768) {
+        $('.articles').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+
+        });
+    }
+
 });
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    if (nameValue === '') {
+        setError(name, 'Name is required');
+    } else {
+        setSuccess(name);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+};
+
+
+
+const nameDiv = document.getElementById('name-control');
+const emailDiv = document.getElementById('email-control');
+function validityCheck() {
+    if (nameDiv.classList.contains('success') && emailDiv.classList.contains('success')) {
+        document.getElementById('form').classList.toggle('valid');
+    }
+
+    else if (nameDiv.classList.contains('error') && emailDiv.classList.contains('error')) {
+        console.log("nay");
+    }
+
+    else if (nameDiv.classList.contains('success') || emailDiv.classList.contains('success')) {
+        console.log('meh');
+    }
+}
